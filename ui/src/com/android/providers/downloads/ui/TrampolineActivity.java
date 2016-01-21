64fe010cecd58ea54083936692897c4f720fe973
pezhan <pezhan@codeaurora.org>
2016-01-21 12:44:01 +0800

    DownloadProvider:Prompts media process has stopeed
    When Rotate DUT,this activity is restarted,so getActivity must be null.
    When use it without judge it,it will leadto crash. On other conditions,
    is may not be null.
    
    Add judgement before use it.
    
    Change-Id: I71a84256bab62ca4d4d5fea443f44ff5509cc422
    CRs-Fixed: 949070

--- ui/src/com/android/providers/downloads/ui/TrampolineActivity.java
+++ ui/src/com/android/providers/downloads/ui/TrampolineActivity.java
@@ -221,7 +221,9 @@ public class TrampolineActivity extends Activity {
         @Override
         public void onDismiss(DialogInterface dialog) {
             super.onDismiss(dialog);
-            getActivity().finish();
+            if(getActivity() != null){
+                getActivity().finish();
+            }
         }
     }
 }
